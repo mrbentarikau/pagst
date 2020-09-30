9aec622c3d564c3cefb36542d9c59856f3834223
caubert <caubert@gmail.com>
2020-09-30 16:31:40 +0300

    shardmanager name change

--- bot/bot.go
+++ bot/bot.go
@@ -387,7 +387,7 @@ func setupShardManager() {
 	ShardManager = dshardmanager.New(common.GetBotToken())
 	ShardManager.LogChannel = int64(connEvtChannel)
 	ShardManager.StatusMessageChannel = int64(connStatusChannel)
-	ShardManager.Name = "YAGPDB"
+	ShardManager.Name = "PAGSTDB"
 	ShardManager.GuildCountsFunc = GuildCountsFunc
 	ShardManager.SessionFunc = func(token string) (session *discordgo.Session, err error) {
 		session, err = discordgo.New(token)
