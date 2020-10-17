b318347032d1bd30fa86b53a8377c899ee5e8a6f
caubert <caubert@gmail.com>
2020-10-17 17:55:43 +0300

    reSplit for templates

--- common/templates/context.go
+++ common/templates/context.go
@@ -534,6 +534,7 @@ func baseContextFuncs(c *Context) {
 	c.ContextFuncs["reFindAll"] = c.reFindAll
 	c.ContextFuncs["reFindAllSubmatches"] = c.reFindAllSubmatches
 	c.ContextFuncs["reReplace"] = c.reReplace
+	c.ContextFuncs["reSplit"] = c.reSplit
 
 	c.ContextFuncs["editChannelTopic"] = c.tmplEditChannelTopic
 	c.ContextFuncs["editChannelName"] = c.tmplEditChannelName
