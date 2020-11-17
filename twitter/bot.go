28a1b4099d8bcef1b9768fcbc6a1ea0333d69813
caubert <caubert@gmail.com>
2020-11-17 16:09:17 +0200

    twitter update, has now mention role ability

--- twitter/bot.go
+++ twitter/bot.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/jonas747/yagpdb/common/mqueue"
-	"github.com/jonas747/yagpdb/twitter/models"
+	"github.com/mrbentarikau/pagst/common/mqueue"
+	"github.com/mrbentarikau/pagst/twitter/models"
 )
 
 func (p *Plugin) Status() (string, string) {
